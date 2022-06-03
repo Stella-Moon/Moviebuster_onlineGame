3e9574ee5cbe7e830905877926144e18bfca8e6d
Stella-Moon <105049892+Stella-Moon@users.noreply.github.com>
2022-06-03 12:15:47 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,47 @@
 // index
 
+// Formulardaten in Datenbank
+
+
+const databaseClient = {
+  // Account Daten
+  data: {
+    url: "https://database-teacher-onlineshop.herokuapp.com/sql",
+        group: "al8",
+        pw: "31349893",
+    sql: ""
+  },
+
+  // HTTP Request an die Datenbank
+  // Mitgegeben wird ein SQL Query als String 
+    
+  executeSqlQuery: async (sql) => {
+    databaseClient.data.sql = sql
+    const response = await fetch(databaseClient.data.url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST",
+      body: JSON.stringify(databaseClient.data)
+    })
+    const result = await response.json()
+    console.log("SQL Query executed: ", result, sql)
+    
+    return result
+  },
+
+  // Mitgegeben wird der Name der Tabelle als String, die Felder als Array und die Werte als Array.
+  // Beispiel: databaseClient.insertInto("customers", ["firstname", "email"], [firstname.value, email.value])
+  insertInto: async (tableName = "gameusers", fields =  ["vorname", "nachname, "email", "gewinnwahl"], values = [vorname.value, nachname.value, email.value, gewinnwahl.value]) => {
+    const sql = `INSERT INTO ${tableName} (${fields.join(",")}) VALUES ("${values.join(",")}")`
+    return await databaseClient.executeSqlQuery(sql)
+  }
+
+}
 
 
 // game in game.js
-
-
-
-
-
-
 
 // shop
 function preisdiffer() {
